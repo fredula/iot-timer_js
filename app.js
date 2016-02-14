95887df399bb09a4cc57346e9891bc77361dd5ce
fredula <fred.thefriedone@gmail.com>
2016-02-14 15:26:38 +0200

    added modules and blinking led

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var fv = require('johnny-five');
+var board = new fv.Board();
+var led;
+
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
+
+board.on('ready', function(){
+	led = new fv.Led(13);
+})
+
+app.post('/ajax', function(req,res){
+	res.type('json');
+	console.log(JSON.stringify(req.body.textValue));
+	res.end(JSON.stringify({}))
+
+	led.toggle();
+})
+
 
 var server = app.listen(8080);
--- public/js/script.js
+++ public/js/script.js
@@ -3,35 +3,59 @@ var App = (function(){
 
 	const baseWidth = 400;
 
-	var growBox = document.querySelector('.innerBox');
-	var count = 0;
+	var growBox = document.querySelectorAll('.innerBox');
+	var circle = document.querySelectorAll('.circle');
+	var innerCircle = document.querySelectorAll('.inner-circle');
+	var colorArray = ['#E74C3C', '#657A7B', '#A93A4A'];
+	var count;
+	var growBoxCount = 0;
 	//var width = 3.3;
 	var time = 10;
 	var increments;
 	var interVal;
 
 	var init = function(){
-		increments = baseWidth/time;
-		console.log(increments, Math.ceil(increments));
 		startTimer();
 	}
 
 	var startTimer = function() {
+		console.log('Starting Timer...');
+		increments = baseWidth/time;
+		count = 0;
 		interVal = setInterval(function(){
 		    count++;
 		    var incrementWidth = increments*count;
 		    if(count <= time){
-		      growBox.style.width = incrementWidth + 'px'; 
+		      growBox[growBoxCount].style.width = incrementWidth + 'px'; 
+		      circle[growBoxCount].style.borderColor = colorArray[growBoxCount];
+		      innerCircle[growBoxCount].style.backgroundColor = colorArray[growBoxCount];
 		      console.log('Length', incrementWidth);
 		    }else{
+		      sendRequest();
 		      killInterval();
 		    }
 		    console.log(count);
 		}, 1000); 
 	}
+
+	var sendRequest = function(){
+		var ajax = new XMLHttpRequest();
+		ajax.onreadystatechange = function() {
+			if (ajax.readyState == 4 && ajax.status == 200) {
+				var response = JSON.parse(ajax.responseText);
+			}
+		};
+		ajax.open('POST', '/ajax', true);
+		ajax.setRequestHeader("Content-type", "application/json");
+		ajax.send(JSON.stringify({'textValue': 'test'}));
+	}
 	
 	var killInterval = function(){
 	  clearInterval(interVal);
+	  if(growBoxCount < growBox.length-1){
+	  	growBoxCount++;
+	  	startTimer();
+	  }
 	};
 
 	return{
